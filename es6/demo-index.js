27556acad6bc72bd066f1d9b710bb6e916cc78f0
feifei <tianbian5@163.com>
2021-11-09 11:59:48 +0800

    使页面能出来，可以看看json不直接import，用fetch的时候怎么才能生效？没仔细研究，先看设计模式

---
+++ es6/demo-index.js
@@ -0,0 +1,4 @@
+import App from "./src/demo/App.js";
+
+let app = new App("app");
+app.init();
--- es6/webpack.dev.config.js
+++ es6/webpack.dev.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/demo/App.js",
+  entry: "./demo-index.js",
   output: {
     path: __dirname,
     filename: "./release/bundle.js", // release 会自动创建
@@ -15,7 +15,7 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, "./release"), // 根目录
     open: true, // 自动打开浏览器
-    port: 9000, // 端口
+    port: 8880, // 端口
     proxy: {
       "/api/*": {
         target: "http://localhost:8880",
